23b5c1bb5645f795e1a62c6349cea8cb237dbe65
Artitzco <96751314+artitzco@users.noreply.github.com>
2022-01-07 21:03:12 -0600

    Update fraping.R

--- R/fraping.R
+++ R/fraping.R
@@ -1,6 +1,472 @@
-source("utilitz.R")
-source("filitz.R")
-source("plotitz.R")
+listToVec <- function(List, index) {vec<-NULL;for (i in index) vec<-c(vec, List[[i]]);return(vec)}
+
+subList<-function(List, index){listy <-list();for (i in 1:length(index)) listy[[i]]<-List[[index[i]]];return(listy)}
+
+split <- function(text) {spl<-NULL; for (i in 1:nchar(text)) spl[i]<-substring(text, i, i); return(spl)}
+
+displit <- function(spl,sp="") {text<-""; for(i in 1:length(spl)) text<-paste(text, spl[i],sp, sep="");return(substring(text,1,nchar(text)-nchar(sp)))}
+
+reparam <- function(x0, x, y) return(y[1]+(x0-x[1])*(y[2]-y[1])/(x[2]-x[1]))
+
+getSeconds <- function() as.numeric(proc.time()[3])
+
+optima <- function(f, interval){
+  n <- length(interval)  
+  if(n==1){
+    optim <- function(f, intX){
+      x1 <- optimize(f, interval = intX[[1]])$minimum
+      return(list(x1 = x1, min = f(x1)))
+    }
+  }else if(n==2){
+    optim <- function(f, intX){
+      f0 <- function(x1, x2) f(x1, x2)
+      f1 <- function(x2) optimize(f0, x2 = x2, interval = intX[[1]])$objective
+      x2 <- optimize(f1, interval = intX[[2]])$minimum
+      x1 <- optimize(f0, x2 = x2, interval = intX[[1]])$minimum
+      return(list(x1 = x1, x2 = x2, min = f0(x1, x2)))
+    }
+  }else if(n==3){
+    optim <- function(f, intX){
+      f0 <- function(x1, x2) f(x1, x2)
+      f1 <- function(x2, x3) optimize(f0, x2 = x2, x3 = x3, interval = intX[[1]])$objective
+      f0 <- function(x1, x2, x3) f(x1, x2, x3)
+      f2 <- function(x3) optimize(f1, x3 = x3, interval = intX[[2]])$objective
+      x3 <- optimize(f2, interval = intX[[3]])$minimum
+      x2 <- optimize(f1, x3 = x3, interval = intX[[2]])$minimum
+      x1 <- optimize(f0, x2 = x2, x3 = x3, interval = intX[[1]])$minimum
+      return(list(x1 = x1, x2 = x2, x3 = x3, min = f0(x1, x2, x3)))
+    }
+  }else if(n==4){
+    optim <- function(f, intX){
+      f0 <- function(x1, x2) f(x1, x2)
+      f1 <- function(x2, x3, x4) optimize(f0, x2 = x2, x3 = x3, x4 = x4, interval = intX[[1]])$objective
+      f0 <- function(x1, x2, x3) f(x1, x2, x3)
+      f2 <- function(x3, x4) optimize(f1, x3 = x3, x4 = x4, interval = intX[[2]])$objective
+      f0 <- function(x1, x2, x3, x4) f(x1, x2, x3, x4)
+      f3 <- function(x4) optimize(f2, x4 = x4, interval = intX[[3]])$objective
+      x4 <- optimize(f3, interval = intX[[4]])$minimum
+      x3 <- optimize(f2, x4 = x4, interval = intX[[3]])$minimum
+      x2 <- optimize(f1, x3 = x3, x4 = x4, interval = intX[[2]])$minimum
+      x1 <- optimize(f0, x2 = x2, x3 = x3, x4 = x4, interval = intX[[1]])$minimum
+      return(list(x1 = x1, x2 = x2, x3 = x3, x4 = x4, min = f0(x1, x2, x3, x4)))
+    }
+  }else if(n==5){
+    optim <- function(f, intX){
+      f0 <- function(x1, x2) f(x1, x2)
+      f1 <- function(x2, x3, x4, x5) optimize(f0, x2 = x2, x3 = x3, x4 = x4, x5 = x5, interval = intX[[1]])$objective
+      f0 <- function(x1, x2, x3) f(x1, x2, x3)
+      f2 <- function(x3, x4, x5) optimize(f1, x3 = x3, x4 = x4, x5 = x5, interval = intX[[2]])$objective
+      f0 <- function(x1, x2, x3, x4) f(x1, x2, x3, x4)
+      f3 <- function(x4, x5) optimize(f2, x4 = x4, x5 = x5, interval = intX[[3]])$objective
+      f0 <- function(x1, x2, x3, x4, x5) f(x1, x2, x3, x4, x5)
+      f4 <- function(x5) optimize(f3, x5 = x5, interval = intX[[4]])$objective
+      x5 <- optimize(f4, interval = intX[[5]])$minimum
+      x4 <- optimize(f3, x5 = x5, interval = intX[[4]])$minimum
+      x3 <- optimize(f2, x4 = x4, x5 = x5, interval = intX[[3]])$minimum
+      x2 <- optimize(f1, x3 = x3, x4 = x4, x5 = x5, interval = intX[[2]])$minimum
+      x1 <- optimize(f0, x2 = x2, x3 = x3, x4 = x4, x5 = x5, interval = intX[[1]])$minimum
+      return(list(x1 = x1, x2 = x2, x3 = x3, x4 = x4, x5 = x5, min = f0(x1, x2, x3, x4, x5)))
+    }
+  }
+  return(optim(f, interval))
+}
+
+color <- function(r=NA, g=NA, b=NA, stand=T){
+  RGB<- newVar()
+  setCol <- function(r=NA, g=NA, b=NA, stand=T){
+    if(is.na(r)){r<-runif(1)}else if(!stand){r<-r/255}
+    if(is.na(g)){g<-runif(1)}else if(!stand){g<-g/255}
+    if(is.na(b)){b<-runif(1)}else if(!stand){b<-b/255}
+    RGB$set(c(r,g,b))
+  }
+  setCol(r, g, b, stand)
+  get<- function() RGB$get()
+  getCol<- function(alpha=1, mult=1){
+    cls<-mult*get()
+    return(rgb(cls[1], cls[2], cls[3], alpha))
+  }
+  combine<- function(col, r=0.5){
+    if(length(r)>1){
+      lst<-list()
+      for (i in 1:length(r)) {
+        cls <- (1-r[i])*get()+r[i]*col$get()
+        lst[[i]] <- color(cls[1], cls[2], cls[3])
+      }
+      return(colorList(col=lst))
+    }
+    cls <- (1-r)*get()+r*col$get()
+    return(color(cls[1], cls[2], cls[3]))
+  }
+  
+  secCol<- function(col, n){
+    r<-if(n!=1)seq(0,1, 1/(n-1)) else 0.5
+    listCol<- list()
+    for (i in 1:n) listCol[[i]]<-combine(col, r[i])
+    return(listCol)
+  }
+  return(list(get=get, getCol=getCol, setCol=setCol, combine=combine, secCol=secCol))
+}
+
+colorList<- function(r=0, g=0, b=0, stand=T, col=NULL){
+  if(is.null(col)){
+    n<-max(length(r),length(g),length(b))
+    r<-r[1+((1:n)-1)%%length(r)]
+    g<-g[1+((1:n)-1)%%length(g)]
+    b<-b[1+((1:n)-1)%%length(b)]
+    colist<-list()
+    for (i in 1:n)
+      colist[[i]]<-color(r[i],g[i],b[i],stand)
+  }else if(is.null(col$colist)){
+    if(is.null(col$get)){
+      colist<-col
+    }else{
+      colist<-list(col)
+    }
+  }else{
+    colist<-col$colist
+  }
+  N<-newCount(cyc=length(colist))
+  col <- function() colist[[1+N$getPlus()]]
+  getCol<- function(...) col()$getCol(...)
+  return(list(colist=colist, col=col, getCol=getCol))
+}
+
+Golbal.List.XnSFfYra5Tkybcgy73S7onBz6ZbleB7MMA7GdBkT <- list()
+
+Clean.Global.List <- function() Golbal.List.XnSFfYra5Tkybcgy73S7onBz6ZbleB7MMA7GdBkT <<- list()
+
+rand <- function(){
+  chars<-c("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","n","m","p","o","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9")
+  randCode<-function() displit(chars[1+floor(length(chars)*runif(round(runif(1,20,35))))])
+  randFile<-function(ext) paste(getwd(),"/file_",randCode(),ext,sep="")
+  rand <<- list(randCode=randCode, randFile=randFile)
+}; rand()
+
+newVar <- function(..., def=NaN){
+  n <- length(Golbal.List.XnSFfYra5Tkybcgy73S7onBz6ZbleB7MMA7GdBkT) + 1
+  value <- def
+  {if(length(list(...)) != 0) value<-list(...)[[1]]}
+  Golbal.List.XnSFfYra5Tkybcgy73S7onBz6ZbleB7MMA7GdBkT[[n]] <<- value
+  return(list(get=function(index=NaN){
+    if(is.na(index[1])){
+      return(Golbal.List.XnSFfYra5Tkybcgy73S7onBz6ZbleB7MMA7GdBkT[[n]])
+    }else{
+      return(Golbal.List.XnSFfYra5Tkybcgy73S7onBz6ZbleB7MMA7GdBkT[[n]][index])
+    }
+  }, 
+  set=function(value, index=NaN){
+    if(is.na(index[1])){
+      Golbal.List.XnSFfYra5Tkybcgy73S7onBz6ZbleB7MMA7GdBkT[[n]]<<-value
+    }else{
+      Golbal.List.XnSFfYra5Tkybcgy73S7onBz6ZbleB7MMA7GdBkT[[n]][index]<<-value 
+    }
+  },
+  length=function() length(Golbal.List.XnSFfYra5Tkybcgy73S7onBz6ZbleB7MMA7GdBkT[[n]])
+  ))
+}
+
+newList <- function(...){
+  n <- length(Golbal.List.XnSFfYra5Tkybcgy73S7onBz6ZbleB7MMA7GdBkT) + 1
+  Golbal.List.XnSFfYra5Tkybcgy73S7onBz6ZbleB7MMA7GdBkT[[n]] <<- list(...)
+  return(list(get=function(index=NaN){
+    if(is.na(index)){
+      return(Golbal.List.XnSFfYra5Tkybcgy73S7onBz6ZbleB7MMA7GdBkT[[n]])
+    }else{
+      return(Golbal.List.XnSFfYra5Tkybcgy73S7onBz6ZbleB7MMA7GdBkT[[n]][[index]])
+    }
+  }, 
+  set=function(value, index)Golbal.List.XnSFfYra5Tkybcgy73S7onBz6ZbleB7MMA7GdBkT[[n]][[index]]<<-value,
+  length=function() length(Golbal.List.XnSFfYra5Tkybcgy73S7onBz6ZbleB7MMA7GdBkT[[n]])
+  ))
+}
+
+newCode <- function(...){
+  code <- newVar("")
+  add <- function(...){
+    cod <- NULL
+    lista <- list(...)
+    for (i in 1:length(lista)) cod[i]<-paste(lista[i])
+    if(code$get()==""){
+      code$set(displit(cod))
+    }else{
+      code$set(paste(code$get(), displit(cod), sep="\n")) 
+    }
+  }
+  if(length(list(...)) != 0){
+    add(...)
+  }
+  run <- function(){
+    file <- rand$randFile(".R")
+    write(code$get(), file)
+    source(file)
+    unlink(file)####Ojo
+  }
+  return(list(add=add, getCode=code$get, run=run))
+}
+
+newFunction <- function(param){
+  code <- newCode()
+  add <- function(...){
+    code$add(...)
+  }
+  getCode <- function(){
+    return(paste("funny.XnSFfYra5Tkybcgy73S7onBz6ZbleB7MMA7GdBkT <- function(",param,"){\n",code$getCode(),"\n}",sep=""))    
+  }
+  f <- function(){
+    file <- rand$randFile(".R")
+    write(getCode(), file)
+    source(file)
+    unlink(file)####Ojo
+    return(funny.XnSFfYra5Tkybcgy73S7onBz6ZbleB7MMA7GdBkT)
+  }
+  return(list(add=add, getCode=getCode, f=f))
+}
+
+newCount <- function(i=0, k=1, cyc=NA){
+  if(!is.na(cyc)) i<-i%%cyc
+  
+  cont <- newVar(i)
+  plus<-function()cont$set(if(!is.na(cyc)) (cont$get()+k)%%cyc else cont$get()+k)
+  less<-function()cont$set(if(!is.na(cyc)) (cont$get()-k)%%cyc else cont$get()-k)
+  get<-cont$get
+  getPlus<-function(){
+    GET<-get()
+    plus()
+    return(GET)
+  }
+  getLess<-function(){
+    GET<-get()
+    less()
+    return(GET)
+  }
+  return(list(plus=plus, less=less, get=get, getPlus=getPlus, getLess=getLess))
+}
+
+getColor <- function(col, alpha=1, brightness=1){
+  return(rgb(col[1]*brightness, col[2]*brightness, col[3]*brightness, alpha))
+}
+
+pol <- function(xlim, ylim, px, py,lx=NA,ly=NA,labX=NA,labY=NA, cex=1, background=color(0.086,0.086,0.086), col.backlines=color(.5,.5,.5), lwd=2, lty=1, alpha=1){
+  mx <- (xlim[2]-xlim[1])/2
+  my <- (ylim[2]-ylim[1])/2
+  x <-c(xlim[1]-mx, xlim[2]+mx)
+  y <-c(ylim[1]-my, ylim[2]+my)
+  if(is.na(lx[1])){
+    lx <- seq(xlim[1],xlim[2],(xlim[2]-xlim[1])/(px-1))  
+  }
+  if(is.na(ly[1])){
+    ly <- seq(ylim[1],ylim[2],(ylim[2]-ylim[1])/(py-1))
+  }
+  if(is.na(labX[1])){
+    labX<-T
+  }
+  if(is.na(labY[1])){
+    labY<-T
+  }
+  polygon(c(x[1],x[1],x[2],x[2]),c(y[1],y[2],y[2],y[1]),col=background$getCol())
+  mabline(v=lx, h=ly, col=col.backlines, N=lwd, lty=lty, alpha=alpha)
+  axis(1,at=lx,labels=labX,cex.axis=cex)
+  axis(2,at=ly,labels=labY,cex.axis=cex)
+}
+
+mplot <- function(x=0,y=0,xlim=NULL, ylim=NULL, px=6, py=6,lx=NA,ly=NA, labX=NA, labY=NA, 
+                  main=NA, xlab=NA, ylab=NA, cex.main=1.3, cex.lab=1.3, cex.axis=1.1, 
+                  col.background=color(0.086,0.086,0.086), col.backlines=color(.5,.5,.5), 
+                  lwd.backlines=2, lty.backlines=1, alpha.backlines=1){
+  if(length(x)>1 & length(y)==1){
+    y <- x
+    x <- 1:length(y)
+  }
+  if(is.null(xlim)){
+    xmin <- min(x)
+    xmax <- max(x)
+    if(xmin==xmax){
+      xlim <-c(x[1]-1, x[1]+1)
+    }else{
+      xlim <-c(xmin, xmax)
+    }
+  }
+  if(is.null(ylim)){
+    ymin <- min(y)
+    ymax <- max(y)
+    if(ymin==ymax){
+      ylim <-c(y[1]-1, y[1]+1)
+    }else{
+      ylim <-c(ymin, ymax)
+    }
+  }
+  plot(x, y, xlim=xlim, ylim=ylim, axes=F, main=main, xlab=xlab, ylab=ylab, cex.main=cex.main, cex.lab=cex.lab)
+  pol(xlim, ylim, px, py, lx, ly, labX,labY, cex.axis, col.background, col.backlines, lwd.backlines, lty.backlines, alpha.backlines)
+}
+
+mlines <- function(x=NULL, y=NULL, lwd=2, col=color(), alpha=1, ...){
+  N<-lwd
+  if(is.null(y)){
+    if(is.null(x)){
+      x<-0
+      y<-0
+    }else{
+      y<-x
+      x<-1:length(y)
+    }
+  }
+  sq<-1
+  if(N!=1) sq<-seq(0, 1, 1.0/N)[-1]
+  
+  for (i in 1:N) {
+    #lines(x, y, lwd=2*(N-i)+1, col="black", ...)
+    lines(x, y, lwd=2*(N-i)+1, col=col$getCol(mult=sq[i], alpha=alpha), ...)
+  }  
+}
+
+mpoints <- function(x=NULL, y=NULL, lwd=2, col=color(), alpha=1, ...){
+  N<-lwd
+  if(is.null(y)){
+    if(is.null(x)){
+      x<-0
+      y<-0
+    }else{
+      y<-x
+      x<-1:length(y)
+    }
+  }
+  sq<-1
+  if(N!=1)sq<-seq(0, 1, 1.0/N)[-1]
+  
+  for (i in 1:N) {
+    #points(x, y, lwd=2*(N-i)+1, col="black", ...)
+    points(x, y, lwd=2*(N-i)+1, col=col$getCol(mult=sq[i], alpha=alpha), ...)
+  }  
+}
+
+mabline <- function(h=NULL,v=NULL, N=2, col=color(1,1,1), alpha=1,...){
+  sq<-1
+  if(N!=1) sq<-seq(0, 1, 1.0/N)[-1]
+  
+  mab <- function(fn, x){
+    for (j in 1:length(x)) {
+      for (i in 1:length(sq)) {
+        fn(x[j], lwd=2*(N-i)+1, col=col$getCol(mult=sq[i], alpha=alpha), ...)
+      }
+    }  
+  }
+  mab(function(x, ...) abline(v=x, ...), v)
+  mab(function(x, ...) abline(h=x, ...), h)
+}
+
+bar <- function(x, y, horizontal=T, col="darkgray", ...){
+  od <- order(x)
+  x <- x[od]
+  y <- y[od]
+  w <- abs(diff(x)/2)
+  w <- c(w[1], w, w[length(w)])
+  x <- x - w[1:length(x)]
+  w <- w + c(w[2:length(w)], 0)
+  x[length(w)] <- x[length(x)] + w[length(x)]
+  for (i in 1:length(y)) {
+    X <- c(x[i],x[i],x[i+1],x[i+1])
+    Y <- c(0, y[i], y[i], 0)
+    if(horizontal){
+      polygon(X,Y, col=col, ...) 
+    }else{
+      polygon(Y,X, col=col, ...)
+    }
+  }
+}
+
+histogram <- function(x, col="darkgray", lwd=2,
+                      main=NA, xlab=NA, ylab=NA, cex.main=1.3, cex.lab=1.3, cex.axis=1.1,
+                      col.background=color(0.086,0.086,0.086), col.backlines=color(.5,.5,.5), 
+                      lwd.backlines=2, lty.backlines=1, alpha.backlines=1){
+  ht<-hist(x, ylim=c(0,1), axes=F, main=main, xlab=xlab, ylab=ylab, cex.main=cex.main, cex.lab=cex.lab)
+  X<-ht$mids
+  Y<-ht$counts/max(ht$counts)
+  pol(c(min(ht$breaks),max(ht$breaks)),c(0,1),6,6, NA, NA, NA, NA, cex.axis, col.background, col.backlines, lwd.backlines, lty.backlines, alpha.backlines)
+  bar(X,Y,lwd=lwd,col=col)
+}
+
+printer <- function(prop=3/4, width=9, height=NULL, size=c(1,1)){
+  ACTIVE <- newVar(T)
+  PROP <- newVar(prop)
+  WIDTH <- newVar(width)
+  HEIGTH <- newVar(if(is.numeric(height)) height else NA)
+  SIZE <- newVar(size)
+  
+  PDF <- function(file=NULL, prop=NULL, width=NULL, height=NULL, size=NULL){
+    if(ACTIVE$get()){
+      if(is.null(file)){
+        i <- 1
+        while(file.exists(paste("output_plot00",i,".pdf",sep=""))){
+          i <- i+1
+        }
+        file<-paste("output_plot00",i,".pdf",sep="")
+      }
+      prop <- if(is.null(prop)) PROP$get() else prop
+      width <- if(is.null(width)) WIDTH$get() else width
+      height <- if(is.null(height)) if(is.na(HEIGTH$get())) prop*width else HEIGTH$get() else height
+      size <- if(is.null(size)) SIZE$get() else size
+      pdf(file = file, width=width*size[2], height=height*size[1])
+    }
+  }
+  OFF <- function(){
+    if(ACTIVE$get()){
+      dev.off()
+    }
+  }
+  
+  setActive <- function(value) ACTIVE$set(value)
+  setProp <- function(prop) PROP$set(prop)
+  setWidth <- function(width) WIDTH$set(width)
+  setHeight <- function(height) HEIGTH$set(if(is.numeric(height)) height else NA)
+  setSize <- function(size) SIZE$set(size)
+  return(list(PDF=PDF, OFF=OFF, setActive=setActive, setProp=setProp, setWidth=setWidth, setHeight=setHeight, setSize=setSize))
+}
+
+tableLine <-function(tim, lin){
+  x<-tim
+  y<-lin
+  newX<-NULL
+  for (i in 1:length(x)) newX<-union(newX, x[[i]])
+  
+  newX<-newX[order(newX)]
+  newY<-NULL
+  
+  for (i in 1:length(x)) {
+    z<-rep(NA, length(newX))
+    
+    for (j in 1:length(newX)) {
+      ind<-sum((1:length(x[[i]]))*(x[[i]]==newX[j]))
+      if(ind!=0) z[j]<-y[[i]][ind]
+    }
+    minx<-min(x[[i]])
+    maxx<-max(x[[i]])
+    
+    x1<-NA
+    y1<-NA
+    for (j in 1:length(newX)) {
+      if(!is.na(z[j])) {
+        x1<-newX[j]
+        y1<-z[j]
+      }else if(newX[j]>=minx & newX[j]<=maxx){
+        k<-j+1
+        while(is.na(z[k])) k<-k+1
+        z[j] <- reparam(newX[j], c(x1, newX[k]),c(y1, z[k]))
+      }
+    }
+    newY<- cbind(newY, z)
+  }
+  ind<-!is.na(apply(newY, 1, sum))
+  return(list(time=newX[ind], table=newY[ind, ], apply=function(fun)apply(newY[ind, ], 1, fun)))
+}
+
+shadow<- function(x1,y1,x2,y2, col=color(0,0,0), alpha=.5, col.border=color(0,0,0), alpha.border=1){
+  inv<-(length(x2):1)
+  x<-c(x2[inv], x1,NA)
+  y<-c(y2[inv], y1,NA)
+  if(is.null(col$getCol)) col<-col[[1]]
+  if(is.null(col.border$getCol)) col.border<-col.border[[1]]
+  polygon(x,y, col=col$getCol(alpha), border= col.border$getCol(alpha.border))
+}
 
 newDataSet <- function(bleach){
   fmin<-1-bleach
